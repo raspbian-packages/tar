raspbian-packages/tar

commit e89c7a45eba7644693870b613386baa45d624e6a

Fix savannah bug #63567
* src/buffer.c (short_read): Increase records_read only if a full
record has been read.

src/buffer.c
@@ -982,7 +982,8 @@ short_read (size_t status)
     }
 
   record_end = record_start + (record_size - left) / BLOCKSIZE;
-  records_read++;
+  if (left == 0)
+    records_read++;
 }
 
 /*  Flush the current buffer to/from the archive.  */

commit 9d64177f1ff91757ef5842d0f28b9862b02db19d

[PATCH] Revert "Fix savannah bug #63567"
From d437ecf75de2d6fdeb2aed6f45c4b3b16373389b Mon Sep 17 00:00:00 2001
Bug-Fedora: https://bugzilla.redhat.com/show_bug.cgi?id=2230127
Bug-Debian: https://bugs.debian.org/748244

Commit e89c7a45eb broke deletion from archives. The reported number
of bytes read is rounded to the nearest record anyway, revert the
commit and document the fact.

Reported by Ed Santiago. See
https://bugzilla.redhat.com/show_bug.cgi?id=2230127

* doc/tar.texi: Document the fact that --totals rounds up the
number of bytes reads to the nearest record.
* src/buffer.c: Revert changes.
* tests/delete06.at: Fix expected status code and stderr.

Gbp-Pq: Name tar-1.35-revert-fix-savannah-bug-633567.patch

src/buffer.c
@@ -987,8 +987,7 @@ short_read (size_t status)
     }
 
   record_end = record_start + (record_size - left) / BLOCKSIZE;
-  if (left == 0)
-    records_read++;
+  records_read++;
 }
 
 /*  Flush the current buffer to/from the archive.  */

tests/delete06.at
@@ -36,7 +36,10 @@ esac
 dd if=archive.tar of=trunc.tar bs=$size count=1 2>/dev/null
 tar --delete 'b/' -f trunc.tar
 ],
-[0],
-[],[],[],[],[gnu, pax])
+[2],
+[],
+[tar: lseek: trunc.tar: Value too large for defined data type
+tar: Exiting with failure status due to previous errors
+],[],[],[gnu, pax])
 
 AT_CLEANUP